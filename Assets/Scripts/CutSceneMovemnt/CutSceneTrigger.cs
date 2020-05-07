13d8cba044a894c703475111b781022c960a04af
Kenton <Webbkenton@yahoo.com>
2020-05-07 18:03:23 -0400

    Added CutScene Trigger
    Also added grid objects for flooring

---
+++ Assets/Scripts/CutSceneMovemnt/CutSceneTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+public class CutSceneTrigger : MonoBehaviour
+{
+    public int Counter;
+    public bool inRange;
+    public GameObject CutTrigger;
+    public PlayableDirector timeline;
+    void Update()
+    {
+        if (Counter >= 1)
+        {
+            CutTrigger.SetActive(false);
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            timeline.Play();
+            Counter++;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Counter++;
+        }
+    }
+}
---
+++ Assets/Scripts/FurnitureScripts/FloorGrid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorGrid : MonoBehaviour
+{
+    public float xStart, yStart;
+    public int collumnLength, rowLength;
+    public float xSpace, ySpace;
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < collumnLength * rowLength; i++)
+        {
+            Instantiate(prefab, new Vector3(xStart + (xSpace * (i % collumnLength)), yStart + (ySpace * (i / collumnLength))), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
