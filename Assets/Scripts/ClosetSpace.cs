22b6a65bfdb572d4a799e75f9e41a9b917a3350d
Kenton <Webbkenton@yahoo.com>
2020-05-24 18:56:15 -0400

    Implemented Simple Closet Space UI
    Currently Needs Icon Spaces to display furniture Information as well as a way to update the list to include the furniture being placed in the closet. Also need a USE statement to be added to the Furniture that will allow it to be placed in Closet.

--- Assets/Scripts/ClosetSpace.cs
+++ Assets/Scripts/ClosetSpace.cs
@@ -1,10 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClosetSpace : MonoBehaviour
 {
     private int closetSpace = 20;
+    public int currentSpace;
+    private int Upgrades;
+    private int nextSpace;
+    public int nextCost = 1000;
+    public Button Costbutton;
+    public Text CostText;
+    public Text SpaceText;
     public List<FurnitureScriptableObject> closetFurniture = new List<FurnitureScriptableObject>();
-    //Each tab on the closet is 20 Spaces for now
+
+    private void Start()
+    {
+        currentSpace = closetSpace;
+        nextSpace = currentSpace + 20;
+    }
+    private void Update()
+    {
+        if (CurrencyManager.instance.CurrentGold < nextCost)
+        {
+            Costbutton.interactable = false;
+        }
+        else
+        {
+            Costbutton.interactable = true;
+        }
+        CostText.text = nextCost.ToString();
+        SpaceText.text = closetSpace.ToString();
+    }
+    public void MoreSpace()
+    {
+        CurrencyManager.instance.CurrentGold -= nextCost;
+        Upgrades++;
+        closetSpace += 20;
+        nextSpace += 20;
+        nextCost += nextCost;
+        Debug.Log(nextCost + "NextCost");
+        Debug.Log(closetSpace + "ClosetSpace");
+        Debug.Log(nextSpace + "nextSpace");
+    }
 }
