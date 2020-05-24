98807623faeb64120f69a1f0b8effc5bf400dbba
Kenton <Webbkenton@yahoo.com>
2020-05-24 19:25:33 -0400

    Fixed issue with mouse setting off transparency

--- Assets/Scripts/TreeTransparent.cs
+++ Assets/Scripts/TreeTransparent.cs
@@ -6,24 +6,41 @@ public class TreeTransparent : MonoBehaviour
 {
     private SpriteRenderer Spriter;
     private Color StartingColor;
+    private float full;
+    private float forty;
+    
     // Start is called before the first frame update
     void Start()
     {
+        full = 100;
+        forty = 40;
+
         Spriter = this.GetComponent<SpriteRenderer>();
         StartingColor = Spriter.color;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Spriter.color = new Color(Spriter.color.r, Spriter.color.g, Spriter.color.b, .4f);
+        if (collision.CompareTag("Player"))
+        {
+            Spriter.color = new Color(Spriter.color.r, Spriter.color.g, Spriter.color.b, .4f);
+        }
+
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        Spriter.color = new Color(Spriter.color.r, Spriter.color.g, Spriter.color.b, .4f);
+        if (collision.CompareTag("Player"))
+        {
+            Spriter.color = new Color(Spriter.color.r, Spriter.color.g, Spriter.color.b, .4f);
+
+        }   
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Spriter.color = StartingColor;
+        if (collision.CompareTag("Player"))
+        {
+            Spriter.color = StartingColor;
+        }
     }
 }
