f3534c8fc2b809b19dc1fa6ffddf15560c304c1e
Kenton <Webbkenton@yahoo.com>
2020-06-15 20:00:03 -0400

    Started the watchtower

---
+++ Assets/Scripts/NPC Scripts/FollowSquares.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowSquares : MonoBehaviour
+{
+
+    public GameObject Square1;
+    public GameObject Square2;
+    public Transform StartingPlayerPosition;
+    private Transform PlayerMovedPosition;
+    private Transform FollowSquarePosition;
+    public bool following;
+
+    private void Start()
+    {
+        PlayerMovedPosition.position =StartingPlayerPosition.position;
+        Square1.transform.position = StartingPlayerPosition.position;
+        Square2.transform.position = StartingPlayerPosition.position;
+    }
+    void Update()
+    {
+        
+        if (following == true)
+        {
+            
+        }
+    }
+}
